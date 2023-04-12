74db79eec49117c7980a9b3ee3a00dd48647c7e9
FrancisMich <franciskasiemobi@gmail.com>
2023-04-12 16:27:07 -0700

    pycodestyle corrected

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,5 +4,6 @@ data structure"""
 
 
 def class_to_json(obj):
-    """A function that returns the dictionary description with simple data structure."""
+    """A function that returns the dictionary descriptio
+    with simple data structure."""
     return obj.__dict__
